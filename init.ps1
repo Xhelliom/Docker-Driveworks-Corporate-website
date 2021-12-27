224a6f16e7b1981a7631f04ea371e63792ce9bdd
Stephane Wetterwald <swetterwald@avenao.com>
2021-12-27 11:55:17 +0100

    remove driveapp config.js (does not exist)

--- init.ps1
+++ init.ps1
@@ -4,10 +4,4 @@
 (get-content -path c:\inetpub\wwwroot\config.js -Raw) -Replace 'REPLACESERVER', $env:SERVER | Set-Content -Path C:\driveworks\example-sites\corporate-website\config.js
 (get-content -path c:\inetpub\wwwroot\config.js -Raw)  -Replace 'REPLACEGROUP', $env:GROUP | Set-Content -Path C:\driveworks\example-sites\corporate-website\config.js
 
-#For DriveApp
-#$JS = get-content -path c:\Driveworks\DriveApp\config.js -Raw
-(get-content -path c:\inetpub\wwwroot\DriveApp\config.js -Raw) -Replace 'REPLACESERVER', $env:SERVER | Set-Content -Path C:\driveworks\DriveApp\config.js
-(get-content -path c:\inetpub\wwwroot\DriveApp\config.js -Raw) -Replace 'REPLACEGROUP', $env:GROUP | Set-Content -Path C:\driveworks\DriveApp\config.js
-(get-content -path c:\inetpub\wwwroot\DriveApp\config.js -Raw) -Replace 'REPLACEDRIVEAPP', $env:DRIVEAPP | Set-Content -Path C:\driveworks\DriveApp\config.js
-
 C:\\ServiceMonitor.exe w3svc
