444c59e7605283f3834d9452123268e4daa23252
Stephane Wetterwald <swetterwald@avenao.com>
2021-12-27 12:26:00 +0100

    modify script so it modify the server and group

--- init.ps1
+++ init.ps1
@@ -1,7 +1,9 @@
 #Read the JS File for Corporate Website
-#$JS = get-content -path c:\Driveworks\example-sites\corporate-website\config.js -Raw
+#$JS = get-content -path c:\inetpub\wwwroot\config.js -Raw
 #replace text in the JS File
-(get-content -path c:\inetpub\wwwroot\config.js -Raw) -Replace 'REPLACESERVER', $env:SERVER | Set-Content -Path C:\driveworks\example-sites\corporate-website\config.js
-(get-content -path c:\inetpub\wwwroot\config.js -Raw)  -Replace 'REPLACEGROUP', $env:GROUP | Set-Content -Path C:\driveworks\example-sites\corporate-website\config.js
+(get-content -path c:\inetpub\wwwroot\config.js -Raw) -Replace 'serverUrl: ""', 'serverUrl: "REPLACESERVER"' | Set-Content -Path c:\inetpub\wwwroot\config.js
+(get-content -path c:\inetpub\wwwroot\config.js -Raw) -Replace 'REPLACESERVER', $env:SERVER | Set-Content -Path c:\inetpub\wwwroot\config.js
+(get-content -path c:\inetpub\wwwroot\config.js -Raw) -Replace 'groupAlias: ""', 'groupAlias: "REPLACEGROUP"' | Set-Content -Path c:\inetpub\wwwroot\config.js
+(get-content -path c:\inetpub\wwwroot\config.js -Raw) -Replace 'REPLACEGROUP', $env:GROUP | Set-Content -Path c:\inetpub\wwwroot\config.js
 
 C:\\ServiceMonitor.exe w3svc
